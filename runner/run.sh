de74a8ad76c5acec4b3d522b9c735d315082703d
Jerome Cance <jerome.cance@gmail.com>
2016-12-07 14:54:37 +0100

    add a second lesson

--- runner/run.sh
+++ runner/run.sh
@@ -6,7 +6,7 @@ cp -r /project/target/* .
 
 cp -r /project/answer/* .
 
-fail=`node_modules/.bin/mocha test/$1 --reporter json | tail -n+2 | jq .stats.failures`
+fail=`node_modules/mocha/bin/mocha test/$1 --reporter json | tail -n+2 | jq .stats.failures`
 
 success=`[ $fail -eq 0 ] && echo true || echo false`
 
