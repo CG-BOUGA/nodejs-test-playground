cacb6d36d35868b02dba0bcde9e6a18070f9292b
Valentin Vetter <valentin@codingame.com>
2017-02-01 15:33:34 +0100

    Reindent utils.js

--- project/utils.js
+++ project/utils.js
@@ -1,16 +1,14 @@
-
-function error(err) {
-	console.error(err);
-}
-
-
-function log(s) {
-    if (typeof(s) === 'string' || s instanceof String) {
-        console.log(s);
-    } else {
-        console.log(JSON.stringify(s));
-    }
-}
-
 exports.error = error;
 exports.log = log;
+
+function error(err) {
+  console.error(err);
+}
+
+function log(s) {
+  if (typeof(s) === 'string' || s instanceof String) {
+    console.log(s);
+  } else {
+    console.log(JSON.stringify(s));
+  }
+}
